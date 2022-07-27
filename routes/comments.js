5af69332307489badc1078315ebc10f943c03ad8
{sdjjm95@naver.com} <sdjjm95@naver.com>
2022-07-28 01:05:41 +0900

    'last'

--- routes/comments.js
+++ routes/comments.js
@@ -8,51 +8,19 @@ const router = express.Router();
 router.get("/", async (req,res) => {
     const { user } = req.query;
     
-    const comments = await Comments.find({ user });
-    // const contents = comments.map((user) => user.user);
-    // console.log(contents)
+    const comments = await Comments.find({ user }).select("-password -guitarId -commentId -__v");
     
-    // const guitars = await Guitar.find({ user : contents })
-    // console.log(guitars)
     res.json({
       comments,
     })
     
     
 });
-// 댓글창 조회 guitarId 값이 같은것만 불러와주세요  
-// router.get("/:guitarId", async (req, res) => {
-//   const comment = await Comments.find()
-//   // console.log(comment) 코멘트 목록 다 불러오기
-//   const guitarIds = comment.map((item) => item.guitarId);       
-//   // console.log(guitarIds)
-//   //  내가 어떤 게시물에 글썼는지 기타 아이디값 3,5,3을 불러옴
-//   const guitars = await Guitar.find({ guitarId: guitarIds })
-//   // console.log(guitars) 
-//   // 위에는 기타포스팅한 글을 불러온다 그리고 기타아이디값을 기준으로 찾음
-
-//   // comment 라는 배열이 있음 근데 맵함수를 씀
-//   // comment1 이라쓴 요소마다 리턴하는데 리턴의 코멘트는 guitarId가 같을때 찾음
-
-//   // 기타스 = 댓글이 있는 애들
-//   res.json({
-//     comment : guitars.map((comment1) => {
-//       return {
-//         postname : comment1.name,
-        
-//         comment : comment.find((comment2) => {
-//           console.log(comment2.guitarId)
-//           return comment1.guitarId === comment2.guitarId})
-//       }
-//     })
-//   })
-// })
-
-// 기타아이디값으로 댓글목록 불러오기
+// 기타 아이디값으로 댓글 조회
 router.get("/:guitarId", async (req, res) => {
   const { guitarId } = req.params;
 
-  const guitars = await Comments.find({ guitarId: Number(guitarId) }).sort({cmtdate : -1});
+  const guitars = await Comments.find({ guitarId: Number(guitarId) }).sort({cmtdate : -1}).select("-password -guitarId -commentId -__v");
   console.log(guitars)
 
   res.json({
@@ -63,18 +31,17 @@ router.get("/:guitarId", async (req, res) => {
 // 댓글 쓰기
 // 내용이 비어있으면 작성 불가
 router.post("/:guitarId", async (req,res) => {
-    const { user, password, content, guitarId, commentId, cmtdate } = req.body;
+    const { user, password, content, guitarId, commentId } = req.body;
+    // console.log(content)
+    const comment = await Comments.find({guitarId})
+    // console.log(comment)
 
-    // const comments = await Comments.find({ guitarId })
-    // // console.log(comments)
-
-    if(content == ""){
+    if(content.length === 0){
       return res.status(400).json({ message : "댓글 내용을 입력해주세요"})
     } else {
       const createdComments = await Comments.create({ content, user, password, guitarId, commentId, cmtdate:Date.now() });
       return res.json({ message: "댓글을 생성하였습니다" })
     
-      res.json({ createdComments });
     }
     
     
@@ -98,30 +65,24 @@ router.delete("/:guitarId/:commentId", async (req, res) => {
 })
 
 // 수정기능
-// 기타아이디랑 댓글코멘트 아이디로 확인하고 수정 없으면 없다라고 알려주기
+// 기타아이디랑 댓글코멘트 아이디로 확인하고 내용이 없다면 없다라고 알려주기
 router.put("/:guitarId/:commentId", async (req, res) => {
   const { guitarId } = req.params;
-  console.log({guitarId})
+  // console.log({guitarId})
   const { commentId } = req.params;
-  console.log({commentId})
-  // const { name } = req.body;
+  // console.log({commentId})
   const { content } = req.body;
   const itGuitar = await Comments.find({ guitarId, commentId });
   console.log(itGuitar)
   
 
-  if(itGuitar.length==0){
-    return res.status(400).json({ success : false , message : "없어요" })
+  if(content.length===0){
+    return res.status(400).json({ success : false , message : "댓글 내용을 입력해주세요" })
   }
   await Comments.updateOne({ guitarId : guitarId, commentId : commentId }, {$set: {content}})
   res.json({ success: true})
 
-  // if (!itGuitar.length){
-  //   await Comments.updateOne({ guitarId : Number(guitarId), commentId : Number(commentId) }, {$set: {content}})
-  // } else {
-  //   return res.json({ message : "수정할게 없음"})
-  // }
-  // res.json({ success : "수정 완료"});
+  
 })
 
 module.exports = router;
--- routes/guitar.js
+++ routes/guitar.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   const { category } = req.query;
   // console.log("category", category)
-  const guitars = await Guitar.find({ category }).sort({cmtdate : -1}).select("-password -_id -__v");
+  const guitars = await Guitar.find({ category }).sort({cmtdate : -1}).select("-category -guitarId -password -_id -__v");
   res.json({
     guitars: guitars
   });
@@ -18,20 +18,14 @@ router.get("/", async (req, res) => {
 router.get("/:guitarId", async (req, res) => {
   const { guitarId } = req.params;
 
-  const [guitars] = await Guitar.find({ guitarId: Number(guitarId) });
+  const [guitars] = await Guitar.find({ guitarId: Number(guitarId) }).select("-guitarId -password -_id -__v");
 
   res.json({
     guitars,
   });
 });
 
-// 주석처리햇음
-// router.post("/:guitarId", (req, res) => {
-//   const { guitarId } = req.params;
-  
-// })
-
-// 포스팅하는거 근데 왜 저건 불 안들어올까? 작동은 되는데
+// 포스팅기능
 router.post("/", async (req, res) => {
 
   const { user, password, guitarId, name, category, date } = req.body;
@@ -47,19 +41,18 @@ router.post("/", async (req, res) => {
     return res.json({ message: "게시글을 생성하였습니다." })
   }
 
-
-  res.json({ createdGuitars });
-
 });
 
 // 삭제기능
 // 비번이 같아야만 작동
 router.delete("/:guitarId", async (req, res) => {
   const { guitarId } = req.params;
-  const itGuitar = await Guitar.find({ guitarId });
-  // console.log(itGuitar)
+  const body = req.body;
+  console.log(body)
+  const [itGuitar] = await Guitar.find({ guitarId });
+  // console.log([itGuitar])
 
-  if (itGuitar.password === {guitarId}.password){
+  if (itGuitar.password === body.password){
     await Guitar.deleteOne({ guitarId });
   } else {
     return res.status(400).json({ message : "본인만 삭제가능"})
@@ -72,12 +65,14 @@ router.delete("/:guitarId", async (req, res) => {
 router.put("/:guitarId", async (req, res) => {
   const { guitarId } = req.params;
   const { name } = req.body;
-  const itGuitar = await Guitar.find({ guitarId });
+  const body = req.body;
+  // console.log(body)
+  const [itGuitar] = await Guitar.find({ guitarId });
 
-  if (itGuitar.password === {guitarId}.password){
+  if (itGuitar.password === body.password){
     await Guitar.updateOne({ guitarId : Number(guitarId) }, {$set: {name}})
   } else {
-    return res.json({ message : "본인만 삭제가능"})
+    return res.status(400).json({ message : "본인만 수정가능"})
   }
   res.json({ success : "수정 완료"});
 })
@@ -86,7 +81,7 @@ router.put("/:guitarId", async (req, res) => {
 // 위에 포스트에서 아이디값을 지정해서 보내는건 권장 ㄴㄴ 왜?why?
 // 고유값을 내 마음대로 지정은 좀;;
 
-// 지금부터는 댓글창
+
 
 
 module.exports = router;
